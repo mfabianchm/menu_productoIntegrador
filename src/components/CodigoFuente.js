b6ca27435b7629c5beb496f7b82e12f1f948bd85
Marcos Chong <mfchongm@hotmail.com>
2022-11-29 09:32:44 -0600

    final changes

--- src/components/CodigoFuente.js
+++ src/components/CodigoFuente.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Codigo = ()  => {
     return (
         <div className="main-content">
-            <h1>Puedes encontrar el código fuente de el proyecto realizado en el siguiente enlace:<a href="https://github.com/mfabianchm/productoIntegrador_UDG">Github</a></h1>
-            <h1>Puedes encontrar el código fuente de este menú en el siguiente enlace:<a href="https://github.com/mfabianchm/menu_productoIntegrador">Menú</a></h1>
-            <h1>Sitio web productivo: <a href="https://producto-integrador-proyecto02udg.onrender.com/">Sitio web productivo</a></h1>
+            <h1 className="codigo-text">Puedes encontrar el código fuente de el proyecto realizado en el siguiente enlace:<a href="https://github.com/mfabianchm/productoIntegrador_UDG">Github</a></h1>
+            <h1 className="codigo-text">Puedes encontrar el código fuente de este menú en el siguiente enlace:<a href="https://github.com/mfabianchm/menu_productoIntegrador">Menú</a></h1>
+            <h1 className="codigo-text">Sitio web productivo: <a href="https://producto-integrador-proyecto02udg.onrender.com/">Sitio web productivo</a></h1>
         </div>
     )
 }
